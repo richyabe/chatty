a7c178e946490dbcc94a83116e03d3f42be0837e
richyabe <richardabe857@gmail.com>
2025-10-20 22:47:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,10 @@ migrate = Migrate(app=app, db=db)
 import routes
 import models
 
+# ✅ Automatically create all tables if they don’t exist
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
     app.run(debug=True)
+
